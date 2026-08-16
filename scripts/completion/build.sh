54f0634261350892ee9bf15c7d794d924d57e804
Jyoti Prakash Saha <jpsaha@users.noreply.github.com>
2026-08-17 01:36:32 +0530

    Create build.sh

---
+++ scripts/completion/build.sh
@@ -0,0 +1,35 @@
+_build_sh_completion() {
+    local cur prev targets
+
+    targets="
+        all
+        config
+        metadata
+        og-generate
+        og-build
+        metadata-check
+        generated
+        imports
+        links
+        og-check
+        prepare-dist
+        prepare-diagnostics
+        html
+        sitemap
+        robots
+        pdf
+        categories
+        book
+        pages-pdf
+        allpdf
+        report
+    "
+
+    cur="${COMP_WORDS[COMP_CWORD]}"
+
+    COMPREPLY=(
+        $(compgen -W "$targets" -- "$cur")
+    )
+}
+
+complete -F _build_sh_completion ./build.sh
